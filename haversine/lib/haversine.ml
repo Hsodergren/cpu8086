e8719c7506ac029af143519021ec8b62fab79c56
stheso <stheso@d10r-ctest.vdc.saab.se>
2024-07-01 16:45:49 +0200

    minor fixes

--- haversine/lib/haversine.ml
+++ haversine/lib/haversine.ml
@@ -5,9 +5,12 @@ let to_radians =
   let c = Float.pi /. 180. in
   fun deg -> c *. deg
 
-let referenceHanversine x0 y0 x1 y1 r =
+let refHaversine x0 y0 x1 y1 r =
   let dy = to_radians (y1 -. y0) in
   let dx = to_radians (x1 -. x0) in
   let y0 = to_radians y0 in
   let y1 = to_radians y1 in
-  r *. Float.(square (sin (dy /. 2.)) +. cos y0 *. cos y1 *. square (sin (dx /. 2.)))
+  let a = Float.(square (sin (dy /. 2.)) +. cos y0 *. cos y1 *. square (sin (dx /. 2.))) in
+  let c = 2. *. Float.asin (Float.sqrt a) in
+  r *. c
+  
